b66b0d917206304480bd92f0a49224fe253e3087
Rustam Shekhmametev <rustam_shekhmametev@epam.com>
2022-01-25 18:55:28 +0300

    refactoring

--- src/main/java/db2/Main.java
+++ src/main/java/db2/Main.java
@@ -4,30 +4,51 @@ import java.sql.*;
 
 public class Main {
 
-    private static final String CREATE_TABLE = "create table if not exists test_table (\n" +
-            " person_id int primary key not null,\n" +
-            " firstName char(255) not null,\n" +
-            " lastName char(255) not null\n" +
-            " );";
-    private static final String INSERT_INTO = "insert into test_table (person_id, firstname, lastname)\n" +
-            "values (1, 'firstname1', 'lastname1')";
-    private static final String SELECT = "select * from test_table";
+    // docker run -itd --name mydb2 --privileged=true -p 50000:50000 -e LICENSE=accept -e DB2INST1_PASSWORD=pass -e DBNAME=testdb -v storage:/database ibmcom/db2
+
+    private static final String DB2_URL = "jdbc:db2://127.0.0.1:50000/testdb";
+    private static final String DB2_USER = "db2inst1";
+    private static final String DB2_PASS = "pass";
+
+    private static final String CREATE_TABLE =
+            "CREATE TABLE IF NOT EXISTS test_table (\n" +
+            "person_id int PRIMARY KEY NOT NULL,\n" +
+            "firstName char(255) NOT NULL,\n" +
+            "lastName char(255) NOT NULL\n" +
+            ");";
+    private static final String INSERT_INTO =
+            "INSERT INTO test_table (person_id, firstname, lastname)\n" +
+            "VALUES (2, 'Name', 'Surname')";
+    private static final String SELECT = "SELECT * FROM test_table";
 
     public static void main(String[] args) {
+//        execute(CREATE_TABLE);
+//        execute(INSERT_INTO);
+        executeQuery(SELECT);
+    }
+
+    private static void execute(String query) {
+        try (Connection connection = DriverManager.getConnection(DB2_URL, DB2_USER, DB2_PASS);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void executeQuery(String query) {
         ResultSet resultSet = null;
+        try (Connection connection = DriverManager.getConnection(DB2_URL, DB2_USER, DB2_PASS);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:db2://127.0.0.1:50000/testdb",
-                "db2inst1",
-                "pass");
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT)) {
-
-//            preparedStatement.execute();
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("firstname"));
-                System.out.println(resultSet.getString("lastname"));
+                String name = resultSet.getString("firstname").trim();
+                String surname = resultSet.getString("lastname").trim();
+                System.out.println(name + " " + surname);
             }
 
         } catch (SQLException e) {
@@ -41,6 +62,5 @@ public class Main {
                 e.printStackTrace();
             }
         }
-
     }
 }
